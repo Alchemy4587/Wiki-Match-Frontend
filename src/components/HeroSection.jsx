71007d149ab5f3b0f750979ab32af3faf822ce53
vanessa082 <wahvanessa22@gmail.com>
2024-08-11 16:07:11 +0100

    worked on the responsiveness of the page

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,9 +4,9 @@ export function HeroSection() {
   return (
     <div className=" w-full flex flex-col gap-10 px-10 items-center">
       <div className="w-full flex flex-col gap-10 px-10 items-center pt-16">
-        <div className="flex flex-col bg-primary-color w-[80%] items-center justify-center font-bold h-44 ">
-          <h2 className="text-nowrap text-2xl">Welcome to</h2>
-          <h1 className="text-6xl ">WikiMatch</h1>
+        <div className="flex flex-col bg-primary-color w-[90%] items-center justify-center font-bold h-44 ">
+          <span className="text-nowrap text- lg:text-2xl">Welcome to</span>
+          <h1 className="text-2xl lg:text-6xl ">WikiMatch</h1>
         </div>
       </div>
 
