03abca434cea159c4fa0b511778b0c21515775f2
vanessa082 <wahvanessa22@gmail.com>
2024-08-11 12:24:36 +0100

    delete, resolving conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import ComboBox from './components/Authocomplete_DropDown'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
      
       <ComboBox/>
+     
       <p className="read-the-docs">
         Select a Language you want to work with.
       </p>
--- src/components/Authocomplete_DropDown.jsx
+++ src/components/Authocomplete_DropDown.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import TextField from '@mui/material/TextField';
+// import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
 export default function ComboBox() {
