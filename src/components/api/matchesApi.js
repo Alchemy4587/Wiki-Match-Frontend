3b45a7c6e1880c965131a963d1ec7da0c7611827
unknown <enowbrenda91106@gmail.com>
2024-08-11 15:52:44 +0100

    second commit

--- src/components/api/matchesApi.js
+++ src/components/api/matchesApi.js
@@ -4,10 +4,10 @@ import { ENDPOINTS } from '../endpoints'
 const url=ENDPOINTS.api + ENDPOINTS.getMatches
 
 
-const fetchMatches = async (languageCode) => {
+const fetchMatches = async (languageObject) => {
 
     try{
-        const response = await axios.get(url + languageCode)
+        const response = await axios.get(url ,{params: languageObject})
         return response.data;
     }
 
