eb8dcb954a88546299ac28fee8ba4970f67bc87a
vanessa082 <wahvanessa22@gmail.com>
2024-08-11 11:32:27 +0100

    styled the auth component

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -9,7 +9,7 @@ export default function Auth() {
       <div>
         <MenuButton className="w-10 h-10 2xl:w-12 2xl:h-12 flex items-center border-2 border-icon-border-color justify-center rounded-full bg-primary-color text-white p-2 hover:bg--primary-color active:bg--primary-color focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all duration-200 ease-in-out">
           <FaRegUser className="text-xl 2xl:text-2xl text-black" />
-        </MenuButton>s
+        </MenuButton>
       </div>
     </Menu>
   );
