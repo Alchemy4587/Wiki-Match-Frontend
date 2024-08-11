faca799a26fb269b7c8853d56100e97724dff619
Mercy Sakah <mercysakah8@gmail.com>
2024-08-11 18:26:52 +0100

    1st Interface Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import React from 'react'
-import Card from './Components/Card'
-
+import React, { useState } from 'react'
+import Home from './pages/home'
+import Search from './pages/search'
+import Navbar from './components/Navbar'
 function App() {
 
+
   return (
-    <>
-<Card/>
-    </>
+  <>
+      <Navbar />
+      <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/search' element={<Search />}/>
+    </Routes> 
+  </>
+    
   )
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,156 +1,207 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MoreDescription from './more-description';
 
-export default function App() {
+export default function Card({toggleLinkModal}) {
+  const [show, setShow] = useState(false)
+  function handleTog () {
+    setShow((prev)=> !prev)
+    if(show) {
+      return <MoreDescription />
+    }
+  }
   return (
-    <div className='flex items-center justify-center flex-wrap gap-16		'>
+    <div className=' grid grid-cols-1 lg:grid-cols-4 gap-4 justify-center	'>
       <div
-      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
-      <img
-        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
-        alt="" />
-      <div className="flex flex-col justify-start p-6">
-        <h5
-          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
-         Label
-        </h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
+        className="flex flex-col gap-4 rounded-lg bg-white shadow-md dark:bg-neutral-200 ">
+        <img
+          className=" h-44 w-full rounded-t-lg object-cover  md:rounded-none md:rounded-l-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Image" />
+        <div className="flex flex-col h-28">
+          <h5
+            className="text-xl font-medium text-neutral-800 dark:text-neutral-50">
+            Label
+          </h5>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+
+        </div>
+    <div className=' pb-2 float-right'>
+    <button onClick={toggleLinkModal} className='font-bold text-2xl lg:text-4xl text-green-500'>+</button>
+    </div>
       </div>
-    </div>
+      <div
+        className="flex flex-col gap-4 rounded-lg bg-white shadow-md dark:bg-neutral-200 ">
+        <img
+          className=" h-44 w-full rounded-t-lg object-cover  md:rounded-none md:rounded-l-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Image" />
+        <div className="flex flex-col h-28">
+          <h5
+            className="text-xl font-medium text-neutral-800 dark:text-neutral-50">
+            Label
+          </h5>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
 
-    <div
-      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
-      <img
-        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
-        alt="" />
-      <div className="flex flex-col justify-start p-6">
-        <h5
-          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
-          Label
-        </h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
+        </div>
+    <div className=' pb-2 float-right'>
+    <button onClick={toggleLinkModal} className='font-bold text-2xl lg:text-4xl text-green-500'>+</button>
+    </div>
       </div>
-    </div>
+      <div
+        className="flex flex-col gap-4 rounded-lg bg-white shadow-md dark:bg-neutral-200 ">
+        <img
+          className=" h-44 w-full rounded-t-lg object-cover  md:rounded-none md:rounded-l-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Image" />
+        <div className="flex flex-col h-28">
+          <h5
+            className="text-xl font-medium text-neutral-800 dark:text-neutral-50">
+            Label
+          </h5>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
 
-    <div
-      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
-      <img
-        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
-        alt="" />
-      <div className="flex flex-col justify-start p-6">
-        <h5
-          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
-          Label
-        </h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
-     
+        </div>
+    <div className=' pb-2 float-right'>
+    <button onClick={toggleLinkModal} className='font-bold text-2xl lg:text-4xl text-green-500'>+</button>
+    </div>
       </div>
-    </div>
+      <div
+        className="flex flex-col gap-4 rounded-lg bg-white shadow-md dark:bg-neutral-200 ">
+        <img
+          className=" h-44 w-full rounded-t-lg object-cover  md:rounded-none md:rounded-l-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Image" />
+        <div className="flex flex-col h-28">
+          <h5
+            className="text-xl font-medium text-neutral-800 dark:text-neutral-50">
+            Label
+          </h5>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
 
-    <div
-      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
-      <img
-        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
-        alt="" />
-      <div className="flex flex-col justify-start p-6">
-        <h5
-          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
-          label
-        </h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
+        </div>
+    <div className=' pb-2 float-right'>
+    <button onClick={toggleLinkModal} className='font-bold text-2xl lg:text-4xl text-green-500'>+</button>
+    </div>
       </div>
-    </div>
+      <div
+        className="flex flex-col gap-4 rounded-lg bg-white shadow-md dark:bg-neutral-200 ">
+        <img
+          className=" h-44 w-full rounded-t-lg object-cover  md:rounded-none md:rounded-l-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Image" />
+        <div className="flex flex-col h-28">
+          <h5
+            className="text-xl font-medium text-neutral-800 dark:text-neutral-50">
+            Label
+          </h5>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
 
-    <div
-      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
-      <img
-        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
-        alt="" />
-      <div className="flex flex-col justify-start p-6">
-        <h5
-          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
-          Label
-        </h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
+        </div>
+    <div className=' pb-2 float-right'>
+    <button onClick={toggleLinkModal} className='font-bold text-2xl lg:text-4xl text-green-500'>+</button>
+    </div>
       </div>
+      <div
+        className="flex flex-col gap-4 rounded-lg bg-white shadow-md dark:bg-neutral-200 ">
+        <img
+          className=" h-44 w-full rounded-t-lg object-cover  md:rounded-none md:rounded-l-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Image" />
+        <div className="flex flex-col h-28">
+          <h5
+            className="text-xl font-medium text-neutral-800 dark:text-neutral-50">
+            Label
+          </h5>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+
+        </div>
+    <div className=' pb-2 float-right'>
+    <button onClick={toggleLinkModal} className='font-bold text-2xl lg:text-4xl text-green-500'>+</button>
     </div>
-
-
-    <div
-      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
-      <img
-        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
-        alt="" />
-      <div className="flex flex-col justify-start p-6">
-        <h5
-          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
-          Label
-        </h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
       </div>
+      <div
+        className="flex flex-col gap-4 rounded-lg bg-white shadow-md dark:bg-neutral-200 ">
+        <img
+          className=" h-44 w-full rounded-t-lg object-cover  md:rounded-none md:rounded-l-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Image" />
+        <div className="flex flex-col h-28">
+          <h5
+            className="text-xl font-medium text-neutral-800 dark:text-neutral-50">
+            Label
+          </h5>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+
+        </div>
+    <div className=' pb-2 float-right'>
+    <button onClick={toggleLinkModal} className='font-bold text-2xl lg:text-4xl text-green-500'>+</button>
     </div>
-
-
-    <div
-      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
-      <img
-        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
-        alt="" />
-      <div className="flex flex-col justify-start p-6">
-        <h5
-          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
-          Label
-        </h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
       </div>
+      <div
+        className="flex flex-col gap-4 rounded-lg bg-white shadow-md dark:bg-neutral-200 ">
+        <img
+          className=" h-44 w-full rounded-t-lg object-cover  md:rounded-none md:rounded-l-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Image" />
+        <div className="flex flex-col h-28">
+          <h5
+            className="text-xl font-medium text-neutral-800 dark:text-neutral-50">
+            Label
+          </h5>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+
+        </div>
+    <div className=' pb-2 float-right'>
+    <button onClick={toggleLinkModal} className='font-bold text-2xl lg:text-4xl text-green-500'>+</button>
     </div>
-
-
-    <div
-      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
-      <img
-        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
-        alt="" />
-      <div className="flex flex-col justify-start p-6">
-        <h5
-          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
-          Label
-        </h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
       </div>
-    </div>
+      <div
+        className="flex flex-col gap-4 rounded-lg bg-white shadow-md dark:bg-neutral-200 ">
+        <img
+          className=" h-44 w-full rounded-t-lg object-cover  md:rounded-none md:rounded-l-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Image"  onClick={toggleLinkModal}/>
+        <div className="flex flex-col h-28">
+          <h5
+            className="text-xl font-medium text-neutral-800 dark:text-neutral-50">
+            Label
+          </h5>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
 
+        </div>
+    <div className=' pb-2 float-right'>
+    <button onClick={toggleLinkModal} className='font-bold text-2xl lg:text-4xl text-green-500'>+</button>
     </div>
+      </div>
+      
+    </div>
+    
   );
 }
