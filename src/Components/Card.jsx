65e0ced3e54ad7bdc7af57398d5b13ce3619ba71
enow john enowbi <johnwashinton88@gmail.com>
2024-08-11 10:40:26 +0100

    items cards

---
+++ src/Components/Card.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='flex items-center justify-center flex-wrap gap-16		'>
+      <div
+      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
+      <img
+        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+        alt="" />
+      <div className="flex flex-col justify-start p-6">
+        <h5
+          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+         Label
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+
+    <div
+      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
+      <img
+        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+        alt="" />
+      <div className="flex flex-col justify-start p-6">
+        <h5
+          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Label
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+
+    <div
+      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
+      <img
+        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+        alt="" />
+      <div className="flex flex-col justify-start p-6">
+        <h5
+          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Label
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+     
+      </div>
+    </div>
+
+    <div
+      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
+      <img
+        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+        alt="" />
+      <div className="flex flex-col justify-start p-6">
+        <h5
+          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          label
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+
+    <div
+      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
+      <img
+        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+        alt="" />
+      <div className="flex flex-col justify-start p-6">
+        <h5
+          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Label
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+
+
+    <div
+      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
+      <img
+        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+        alt="" />
+      <div className="flex flex-col justify-start p-6">
+        <h5
+          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Label
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+
+
+    <div
+      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
+      <img
+        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+        alt="" />
+      <div className="flex flex-col justify-start p-6">
+        <h5
+          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Label
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+
+
+    <div
+      className="flex flex-col rounded-lg bg-white shadow-2xl dark:bg-neutral-700 md:max-w-xl md:flex-row">
+      <img
+        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+        alt="" />
+      <div className="flex flex-col justify-start p-6">
+        <h5
+          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Label
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+
+    </div>
+  );
+}
